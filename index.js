c4d80c821a42450d7047156ddf84c384082627d5
shahidul-islam <shahidulislam47326@gmail.com>
2026-01-04 19:45:01 +0600

    app patch add

--- index.js
+++ index.js
@@ -72,6 +72,19 @@ async function run() {
       const result = await checkOutCollection.deleteOne(query);
       res.send(result);
     });
+
+    app.patch("/checkOut/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateCourse = req.body;
+      const updateDoc = {
+        $set: {
+          status: updateCourse.status,
+        },
+      };
+      const result = await checkOutCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
